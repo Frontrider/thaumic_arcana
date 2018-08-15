a0547962620d1529161e87990c5dc1f6c3302f1a
frontrider <kisandrasgabor@gmail.com>
2018-08-15 21:32:04 +0200

    test updated to list aspects that closely resemble the incorrectly spelled one

--- src/test/java/testutil/Assertions.java
+++ src/test/java/testutil/Assertions.java
@@ -1,16 +1,37 @@
 package testutil;
 
+import org.apache.commons.text.similarity.LevenshteinDistance;
 import org.opentest4j.AssertionFailedError;
 import thaumcraft.api.aspects.Aspect;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Assertions {
 
+
+
     public static void assertAspectNameValid(String aspect){
+        Map<Integer, String> aspectmaching = new HashMap<>();
+
         for (String aspectName : Aspect.aspects.keySet()) {
             if(aspect.equals(aspectName))
                 return;
+            Integer distance = LevenshteinDistance.getDefaultInstance().apply(aspect, aspectName);
+            aspectmaching.put(distance,aspectName);
+
         }
-        throw new AssertionFailedError("Invalid aspect found: "+ aspect);
+        List<String> matching = aspectmaching.entrySet()
+                .stream()
+                .filter(integerStringEntry -> integerStringEntry.getKey() < 7)
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+
+
+        throw new AssertionFailedError("Invalid aspect found: "+ aspect+" possible matches: "+matching);
     }
+
 
 }
