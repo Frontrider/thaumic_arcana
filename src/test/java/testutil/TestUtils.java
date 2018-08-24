25dae1a07953b1a690dba9fcb9553ee632fd6b83
frontrider <kisandrasgabor@gmail.com>
2018-08-24 19:36:07 +0200

    added licence header for clarification.

--- src/test/java/testutil/TestUtils.java
+++ src/test/java/testutil/TestUtils.java
@@ -4,6 +4,21 @@ import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+/**
+ *
+ * This class is from stackoverflow, which means that it is under MIT license.I can't find the original, so I include an another header to clarify it.
+* Feel free to use it, because it's a powerfull tool at bypassing some of the bad designs of minecraft.
+ *
+ * Copyright 2018 Kis András
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * */
+
 public class TestUtils {
     // get a static class value
     public static Object reflectValue(Class<?> classToReflect, String fieldNameValueToFetch) {
